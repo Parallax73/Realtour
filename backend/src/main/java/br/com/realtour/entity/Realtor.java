e23454ae570f265d5a5681bb55fc295a368c31f0
Parallax <narakagaara@gmail.com>
2025-03-25 19:57:39 -0300

    Added realtor entity

---
+++ backend/src/main/java/br/com/realtour/entity/Realtor.java
@@ -0,0 +1,34 @@
+package br.com.realtour.entity;
+
+import br.com.realtour.util.RegisterRealtorDTO;
+import br.com.realtour.util.UserRoles;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+@Getter
+@Setter
+public class Realtor {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String creci;
+    private UserRoles role;
+
+    public Realtor(@NotNull RegisterRealtorDTO dto){
+        this.username = dto.username();
+        this.password = dto.password();
+        this.creci = dto.creci();
+        this.role = UserRoles.REALTOR;
+    }
+
+}
