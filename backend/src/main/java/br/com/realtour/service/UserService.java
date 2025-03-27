c71be9a20c93f572b138892859a65036f96d4418
Parallax <narakagaara@gmail.com>
2025-03-27 12:06:41 -0300

    Updated createClient method

--- backend/src/main/java/br/com/realtour/service/UserService.java
+++ backend/src/main/java/br/com/realtour/service/UserService.java
@@ -4,6 +4,7 @@ import br.com.realtour.entity.Client;
 import br.com.realtour.entity.Realtor;
 import br.com.realtour.repository.ClientRepository;
 import br.com.realtour.repository.RealtorRepository;
+import br.com.realtour.util.Estados;
 import br.com.realtour.util.RegisterClientDTO;
 import br.com.realtour.util.RegisterRealtorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,20 +20,18 @@ public class UserService {
     RealtorRepository realtorRepository;
 
     public Client createClient(RegisterClientDTO dto) {
-        try {
-            Client client = new Client(dto);
-            return clientRepository.save(client);
-        } catch (RuntimeException e) {
-            throw new RuntimeException("Client already exists with this email");
+
+        if (clientRepository.existsByUsername(dto.username())){
+            throw new IllegalArgumentException("Username already in use");
         }
+        Client client = new Client(dto);
+        return clientRepository.save(client);
+
     }
 
     public Realtor createRealtor(RegisterRealtorDTO dto){
 
-        //Fazer validação de estado e número (criar interface com as federações e fazer a validação).
-
-
-            if (dto.creci().length()<9){
+            if (dto.creci().length()<9 || !validateCreci(dto.creci())){
                 throw new IllegalArgumentException("Creci is invalid, check the number or state.");
             }
             if (realtorRepository.existsByUsername(dto.username())){
@@ -41,4 +40,9 @@ public class UserService {
             Realtor client = new Realtor(dto);
             return realtorRepository.save(client);
         }
+
+        private boolean validateCreci(String creci){
+           return Estados.isValidState(creci.substring(0,2).toUpperCase()) && !realtorRepository.existsByCreci(creci);
+        }
+
 }
