49c6a242bb2b9c3a175d74e728aa936df008c278
Parallax <narakagaara@gmail.com>
2025-03-27 12:05:06 -0300

    Added existsByUsername method

--- backend/src/main/java/br/com/realtour/repository/ClientRepository.java
+++ backend/src/main/java/br/com/realtour/repository/ClientRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ClientRepository  extends MongoRepository<Client, String> {
 
-
+    boolean existsByUsername(String username);
 }
