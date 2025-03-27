237743a01ca230c1a8efbadc2e61ad0343a8ccb9
Parallax <narakagaara@gmail.com>
2025-03-27 12:06:12 -0300

    Updated registerClient method

--- backend/src/main/java/br/com/realtour/controller/UserController.java
+++ backend/src/main/java/br/com/realtour/controller/UserController.java
@@ -1,6 +1,7 @@
 package br.com.realtour.controller;
 
 
+import br.com.realtour.entity.Client;
 import br.com.realtour.entity.Realtor;
 import br.com.realtour.service.UserService;
 import br.com.realtour.util.RegisterClientDTO;
@@ -9,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,17 +23,23 @@ public class UserController {
 
 
 
-    @GetMapping("create-client")
-    public ResponseEntity<HttpStatus> registerClient(@RequestBody RegisterClientDTO dto){
-        this.service.createClient(dto);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    @PostMapping("create-client")
+    public ResponseEntity<?> registerClient(@RequestBody RegisterClientDTO dto){
+        try {
+            Client client = this.service.createClient(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Created with success");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+        }
     }
 
     @PostMapping("register-realtor")
     public ResponseEntity<?> registerRealtor(@RequestBody RegisterRealtorDTO dto) {
         try {
             Realtor realtor = this.service.createRealtor(dto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(realtor);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Created with success");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
