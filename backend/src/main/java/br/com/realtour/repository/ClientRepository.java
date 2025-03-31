3978d5ba4afd441f924aa9f149bb835e4d4a2377
Parallax <narakagaara@gmail.com>
2025-03-31 19:42:05 -0300

    Added findBByUsername method

--- backend/src/main/java/br/com/realtour/repository/ClientRepository.java
+++ backend/src/main/java/br/com/realtour/repository/ClientRepository.java
@@ -3,9 +3,12 @@ package br.com.realtour.repository;
 
 
 import br.com.realtour.entity.Client;
+import br.com.realtour.entity.Realtor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ClientRepository  extends MongoRepository<Client, String> {
 
     boolean existsByUsername(String username);
+
+    Realtor findByUsername(String username);
 }
