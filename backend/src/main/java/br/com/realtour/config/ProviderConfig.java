c8a1f2a3fbacb58e9bef67f86540728c8f68baa3
Parallax73 <narakagaara@gmail.com>
2025-04-25 17:04:38 -0300

    Changed package to config package

---
+++ backend/src/main/java/br/com/realtour/config/ProviderConfig.java
@@ -0,0 +1,29 @@
+package br.com.realtour.config;
+
+import br.com.realtour.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProviderConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public ProviderConfig(CustomUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+}
---
+++ backend/src/main/java/br/com/realtour/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package br.com.realtour.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private JwtAuthFilter filter;
+
+    final AuthenticationProvider provider;
+
+    public SecurityConfig(JwtAuthFilter filter,AuthenticationProvider provider) {
+        this.provider = provider;
+        this.filter = filter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+         http.authorizeHttpRequests(authorize ->
+                 authorize.requestMatchers(
+                         "/api/v1/users/register-realtor",
+                                 "/api/v1/users/login-realtor")
+                         .permitAll()
+                         .anyRequest()
+                         .authenticated())
+                 .authenticationProvider(provider)
+                 .csrf(AbstractHttpConfigurer::disable)
+                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
+
