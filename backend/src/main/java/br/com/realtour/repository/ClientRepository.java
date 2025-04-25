45c1008d640894077a77a64a6472be985df5b06e
Parallax <narakagaara@gmail.com>
2025-04-24 21:31:52 -0300

    Changed findByUsername return to Optional

--- backend/src/main/java/br/com/realtour/repository/ClientRepository.java
+++ backend/src/main/java/br/com/realtour/repository/ClientRepository.java
@@ -3,12 +3,15 @@ package br.com.realtour.repository;
 
 
 import br.com.realtour.entity.Client;
-import br.com.realtour.entity.Realtor;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 
 public interface ClientRepository  extends MongoRepository<Client, String> {
 
     boolean existsByUsername(String username);
 
-    Realtor findByUsername(String username);
+    Optional<Client> findByUsername(String username);
 }
