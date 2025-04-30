871bb872e52400b1af9308fcf9ae6ee645e021f2
Parallax73 <narakagaara@gmail.com>
2025-04-30 18:06:57 -0300

    Created navbar component

---
+++ frontend/src/app/components/menu-navbar/menu-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-navbar',
+  imports: [],
+  templateUrl: './menu-navbar.component.html',
+  styleUrl: './menu-navbar.component.scss'
+})
+export class MenuNavbarComponent {
+
+}
