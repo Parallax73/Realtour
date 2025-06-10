19061b071adf210258257863597440dc692f4c19
Parallax73 <narakagaara@gmail.com>
2025-06-10 14:38:49 -0300

    Updated chat document

--- backend/src/main/java/br/com/realtour/entity/Chat.java
+++ backend/src/main/java/br/com/realtour/entity/Chat.java
@@ -1,4 +1,27 @@
 package br.com.realtour.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Chat {
+
+    @Id
+    public String id;
+    Realtor realtor;
+    Client client;
+    List<Message> messages = new ArrayList<>();
+
+    public void addMessage(Message message){
+        this.messages.add(message);
+    }
 }
