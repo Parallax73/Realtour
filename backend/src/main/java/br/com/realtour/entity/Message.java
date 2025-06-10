0a0ba77c8ad8c1038d7efab9b77c932096b40dde
Parallax73 <narakagaara@gmail.com>
2025-06-10 15:20:24 -0300

    Updated message entity

--- backend/src/main/java/br/com/realtour/entity/Message.java
+++ backend/src/main/java/br/com/realtour/entity/Message.java
@@ -1,4 +1,23 @@
 package br.com.realtour.entity;
 
+
+import br.com.realtour.util.Sender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.Date;
+
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+    public String message;
+    public Sender sender;
+    public Date sended;
 }
