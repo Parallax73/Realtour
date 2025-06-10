42c180ec262f543718e4ca0136c1c81aa6d3829c
Parallax73 <narakagaara@gmail.com>
2025-06-10 15:29:35 -0300

    Added broker and endpoint methods for WebSocket

--- backend/src/main/java/br/com/realtour/config/WebSocketConfig.java
+++ backend/src/main/java/br/com/realtour/config/WebSocketConfig.java
@@ -1,4 +1,26 @@
 package br.com.realtour.config;
 
-public class WebSocketConfig {
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
 }
