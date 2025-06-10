480d638cfdc08fb098b064eade51654a6b352914
Parallax73 <narakagaara@gmail.com>
2025-06-10 15:06:38 -0300

    Refactored to extend sender interface

--- backend/src/main/java/br/com/realtour/entity/Client.java
+++ backend/src/main/java/br/com/realtour/entity/Client.java
@@ -1,12 +1,12 @@
 package br.com.realtour.entity;
 
 import br.com.realtour.util.RegisterClientDTO;
+import br.com.realtour.util.Sender;
 import br.com.realtour.util.UserRoles;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-/*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;*/
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -18,20 +18,25 @@ import java.util.Set;
 @Document
 @Getter
 @Setter
-public class Client {
+public class Client implements Sender {
 
     @Id
     private String id;
+    private String email;
     private String username;
     private String password;
     private Set<Role> roles = new HashSet<>();
     private List<Unit> savedUnits;
 
-   /* BCryptPasswordEncoder encoder;*/
 
     public Client(@NotNull RegisterClientDTO dto){
         this.username = dto.username();
+        this.email = dto.email();
         this.savedUnits = new ArrayList<>();
         roles.add(new Role(UserRoles.ROLE_USER));
+    }
+
+    public void addUnits(Unit unit){
+        this.savedUnits.add(unit);
     }
 }
