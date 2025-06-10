305f236e8b6203b9df5023a153260bb755300515
Parallax73 <narakagaara@gmail.com>
2025-06-10 15:07:33 -0300

    Refactored to use ReactiveMongoRepository

--- backend/src/main/java/br/com/realtour/repository/ClientRepository.java
+++ backend/src/main/java/br/com/realtour/repository/ClientRepository.java
@@ -1,17 +1,13 @@
 package br.com.realtour.repository;
 
-
-
 import br.com.realtour.entity.Client;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
-import java.util.Optional;
-
-
-public interface ClientRepository  extends MongoRepository<Client, String> {
-
-    boolean existsByUsername(String username);
-
-    Optional<Client> findByUsername(String username);
+@Repository
+public interface ClientRepository extends ReactiveMongoRepository<Client, String> {
+    Mono<Boolean> existsByUsername(String username);
+    Mono<Client> findByUsername(String username);
+    Mono<Client> findByEmail(String email);
 }
