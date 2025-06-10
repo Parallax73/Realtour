65730917495eb1964176235f2e668e51db152f45
Parallax73 <narakagaara@gmail.com>
2025-06-10 15:25:32 -0300

    Added email field

--- backend/src/main/java/br/com/realtour/util/RegisterClientDTO.java
+++ backend/src/main/java/br/com/realtour/util/RegisterClientDTO.java
@@ -1,9 +1,14 @@
 package br.com.realtour.util;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record RegisterClientDTO(
+        @NotBlank
+        @Email
+        String email,
         @NotEmpty
         @Size(min = 4, max = 15) String username,
         @NotEmpty
--- backend/src/main/java/br/com/realtour/util/RegisterRealtorDTO.java
+++ backend/src/main/java/br/com/realtour/util/RegisterRealtorDTO.java
@@ -1,14 +1,20 @@
 package br.com.realtour.util;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRealtorDTO(
+        @NotBlank
+        @Email
+        String email,
         @NotEmpty
         @Size(min = 4, max = 15) String username,
         @NotEmpty
         @Size(min = 4,max = 15)
         String password,
         @NotEmpty
+        @Size(min = 8, max = 9)
         String creci) {
 }
