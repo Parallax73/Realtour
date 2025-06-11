2ed804c7311b1d4a86b43cd72bdbfddfbf50ed3c
Parallax73 <narakagaara@gmail.com>
2025-06-11 19:18:42 -0300

    Added chat path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ChatPageComponent } from './pages/chat-page/chat-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent},
-    {path: 'login', component: LoginPageComponent}
+    {path: 'login', component: LoginPageComponent},
+    {path: 'chat', component: ChatPageComponent}
 ];
