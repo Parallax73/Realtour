9bbc7324987f2553b80be1f112e36661c17b2ea0
Parallax73 <narakagaara@gmail.com>
2025-06-11 19:20:21 -0300

    Added findById

--- backend/src/main/java/br/com/realtour/repository/UnitRepository.java
+++ backend/src/main/java/br/com/realtour/repository/UnitRepository.java
@@ -2,9 +2,13 @@ package br.com.realtour.repository;
 
 import br.com.realtour.entity.Realtor;
 import br.com.realtour.entity.Unit;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface UnitRepository extends ReactiveMongoRepository<Unit,String> {
     Flux<Unit> findByRealtor(Realtor realtor);
+
+    Mono<Unit> findById(@NotNull String id);
 }
