b8cc75ea1d87f0c51d9e00cb5853f2308790abb3
Parallax73 <narakagaara@gmail.com>
2025-06-11 19:20:09 -0300

    Added findByClientAndRealtorAndUnit

--- backend/src/main/java/br/com/realtour/repository/ChatRepository.java
+++ backend/src/main/java/br/com/realtour/repository/ChatRepository.java
@@ -1,9 +1,16 @@
 package br.com.realtour.repository;
 
 import br.com.realtour.entity.Chat;
+import br.com.realtour.entity.Client;
+import br.com.realtour.entity.Realtor;
+import br.com.realtour.entity.Unit;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+
+    Mono<Chat> findByClientAndRealtorAndUnit(Client client, Realtor realtor, Unit unit);
 }
