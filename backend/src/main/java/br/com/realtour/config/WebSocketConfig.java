d28818080d9def9677fce5622d286d48705bdc9d
Parallax73 <narakagaara@gmail.com>
2025-06-11 19:21:24 -0300

    Refactored to chat per unit

--- backend/src/main/java/br/com/realtour/config/WebSocketConfig.java
+++ backend/src/main/java/br/com/realtour/config/WebSocketConfig.java
@@ -1,26 +1,70 @@
 package br.com.realtour.config;
 
-import org.springframework.messaging.converter.MessageConverter;
+import br.com.realtour.service.JwtService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
-import java.util.List;
+@Configuration
+@EnableWebSocketMessageBroker
+@Slf4j
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
-
+    @Autowired
+    private JwtService jwtService;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("http://localhost:4200")
                 .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/user");
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.setUserDestinationPrefix("/user");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/user");
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String token = accessor.getFirstNativeHeader("Authorization");
+                    log.debug("WebSocket connection with token: {}", token);
+
+                    if (token != null && token.startsWith("Bearer ")) {
+                        token = token.substring(7);
+                        try {
+                            String username = jwtService.extractUsername(token);
+                            if (username != null) {
+                                accessor.setUser(() -> username);
+                                log.info("User '{}' authenticated for WebSocket.", username);
+                            }
+                        } catch (Exception e) {
+                            log.error("WebSocket authentication error: {}", e.getMessage());
+                        }
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
