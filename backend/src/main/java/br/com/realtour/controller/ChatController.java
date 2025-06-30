d3dfe08d2f1008473da0d96fb5c4ef2a6e2b0a10
parallax <narakagaara@gmail.com>
2025-06-30 20:10:52 -0300

    Added get-chats method

--- backend/src/main/java/br/com/realtour/controller/ChatController.java
+++ backend/src/main/java/br/com/realtour/controller/ChatController.java
@@ -5,9 +5,12 @@ import br.com.realtour.service.ChatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/chats")
@@ -21,8 +24,7 @@ public class ChatController {
     @PostMapping("/create")
     public Mono<ResponseEntity<Chat>> createChat(
             @CookieValue("SecureJWT") String token) {
-
-        String unitId = "685ad63c97196f6aa86d3530";
+        String unitId = "685efdda607d1c5477d1b15b";
         String realtorUsername = "papa1@gmail.com";
         return chatService.createChat(token, unitId, realtorUsername)
                 .map(ResponseEntity::ok)
@@ -31,4 +33,22 @@ public class ChatController {
                     return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
                 });
     }
+
+    @GetMapping("/get-chats")
+    public Mono<ResponseEntity<List<Chat>>> getChats(@CookieValue("SecureJWT") String token) {
+        log.info("Received request to get chats with token: {}", token);
+        return chatService.getUserChats(token)
+                .collectList()
+                .map(chats -> {
+                    log.info("Retrieved {} chats", chats.size());
+                    return ResponseEntity.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(chats);
+                })
+                .onErrorResume(e -> {
+                    log.error("Failed to fetch chats: {}", e.getMessage(), e);
+                    return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+                });
+    }
+
 }
