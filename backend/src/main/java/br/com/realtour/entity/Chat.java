7b8e8384275e7abfd2514c047a331acb29634507
parallax <narakagaara@gmail.com>
2025-06-30 20:08:05 -0300

    Fixed DBRef Websocket error

--- backend/src/main/java/br/com/realtour/entity/Chat.java
+++ backend/src/main/java/br/com/realtour/entity/Chat.java
@@ -2,11 +2,9 @@ package br.com.realtour.entity;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Builder;
 import lombok.Data;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -16,12 +14,12 @@ import java.util.List;
 public class Chat {
     @Id
     private String id;
-    @DBRef
-    private Client client;
-    @DBRef
-    private Realtor realtor;
-    @DBRef
-    private Unit unit;
+    private String clientEmail;
+    private String clientName;
+    private String realtorEmail;
+    private String realtorName;
+    private String unitId;
+    private String unitAddress;
+    private String unitNumber;
     private List<Message> messages;
-
 }
