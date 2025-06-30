93086b5c7330acf5151ba4c59871f616b979d324
parallax <narakagaara@gmail.com>
2025-06-30 20:11:25 -0300

    Added findByClientEmailOrRealtorEmail and changed Params of findByClientEmailAndRealtorEmailAndUnitId

--- backend/src/main/java/br/com/realtour/repository/ChatRepository.java
+++ backend/src/main/java/br/com/realtour/repository/ChatRepository.java
@@ -6,11 +6,13 @@ import br.com.realtour.entity.Realtor;
 import br.com.realtour.entity.Unit;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 
 
-    Mono<Chat> findByClientAndRealtorAndUnit(Client client, Realtor realtor, Unit unit);
+    Mono<Chat> findByClientEmailAndRealtorEmailAndUnitId(String clientEmail, String realtorEmail, String unitID);
+    Flux<Chat> findByClientEmailOrRealtorEmail(String email,String email2);
 }
