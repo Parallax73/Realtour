4b954c40afd5cf80cda367b5ae42634a8d912377
parallax <narakagaara@gmail.com>
2025-07-02 15:04:51 -0300

    Added Unit page path to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ChatPageComponent } from './pages/chat-page/chat-page.component';
+import { UnitPageComponent } from './pages/unit-page/unit-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent},
     {path: 'login', component: LoginPageComponent},
-    {path: 'chat', component: ChatPageComponent}
+    {path: 'chat', component: ChatPageComponent},
+    {path: 'units', component: UnitPageComponent}
 ];
