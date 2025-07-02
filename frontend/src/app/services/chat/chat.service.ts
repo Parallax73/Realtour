42bf8ace461c3381bd5549744a7d1bbdb5656e48
parallax <narakagaara@gmail.com>
2025-07-02 15:05:38 -0300

    Refactored, removing headers and added getChat method

--- frontend/src/app/services/chat/chat.service.ts
+++ frontend/src/app/services/chat/chat.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Chat } from '@app/models/chat';
+import { Message } from '@app/models/message';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,44 +13,46 @@ export class ChatService {
 
   constructor(private http: HttpClient) { }
 
-
-  getChats(): Observable<any> {
-    const token = this.getToken();
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
-
+  getChatList(): Observable<any> {
     return this.http.get<any>(
-      `${this.apiUrl}/get-chats`,
-      {
-        headers,
-        withCredentials: true
-      }
+      `${this.apiUrl}/get-chats`
     );
   }
 
-  getOrCreateChat(unitId: string, realtorUsername: string): Observable<any> {
-    const token = this.getToken();
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
-
+  createChat(unitId: string, realtorUsername: string): Observable<any> {
     return this.http.post<any>(
-        this.apiUrl + '/create',
-        {},
-        {
-            headers,
-            params: { unitId, realtorUsername }
-        }
+      this.apiUrl + '/create',
+      {}
     );
   }
 
-  private getToken(): string {
-    return document.cookie
-      .split('; ')
-      .find(row => row.startsWith('SecureJWT='))
-      ?.split('=')[1] || '';
+
+  getChat(id: string): Observable<Chat> {
+    return this.http.get<any>(`${this.apiUrl}/get/${id}`).pipe(
+        map(data => ({
+            id: data._id,
+            clientEmail: data.clientEmail,
+            clientName: data.clientName,
+            realtorEmail: data.realtorEmail,
+            realtorName: data.realtorName,
+            unitId: data.unitId,
+            unitAddress: data.unitAddress || null,
+            unitNumber: data.unitNumber,
+            messages: data.messages.map((message: Message) => ({
+                sender: message.sender,
+                content: message.content,
+                timestamp: message.timestamp
+            }))
+        }))
+    );
+}
+
+  sendMessage(chatId: string, messageText: string): Observable<any> {
+    return this.http.post<any>(
+      `${this.apiUrl}/${chatId}/messages`,
+      { content: messageText }
+    );
   }
+
+  
 }
